a8448c3aac5706df42b5fd0166c133294122ecc7
072ashin <leiqing126@gmail.com>
2016-04-02 22:03:34 -0500

    Update redirect.php

--- application/third_party/rbac/views/redirect.php
+++ application/third_party/rbac/views/redirect.php
@@ -5,11 +5,11 @@
 		
 			<div class="col-sm-offset-3 col-sm-6">
 					<div class="panel panel-<?php if($type=="error")echo "danger";else echo "success";?>">
-						<div class="panel-heading"><?php if($type=="error")echo "错误操作";else echo "操作成功";?></div>
+						<div class="panel-heading"><?php if($type=="error")echo "Error";else echo "Success!";?></div>
 						<div class="panel-body">
 						
 						<h1><?php echo $contents; ?></h1>
-    					<h4><span id="cnt"><?php echo $time; ?></span>秒钟后自动跳转！【<a href="<?php echo $url; ?>">立即跳转</a>】</h4>
+    					<h4><span id="cnt">Please wait for<?php echo $time; ?>seconds</span>【<a href="<?php echo $url; ?>">No wait!</a>】</h4>
 						<br/>
 						
 						</div>
