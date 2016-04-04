38554cdc49ab5b90375960ddf890ece68b029541
072ashin <leiqing126@gmail.com>
2016-04-04 16:39:40 -0500

    Update hooks.php

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -11,3 +11,28 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+//RBAC权限验证
+$hook['post_controller_constructor'] = array(
+		'class'    => 'Rbac',
+		'function' => 'aoto_verify',
+		'filename' => 'rbac_hook.php',
+		'filepath' => 'third_party/rbac/hooks',
+		'params'   => '',
+);
+$hook['display_override'] = array(
+		'class'    => 'Rbac',
+		'function' => 'view_override',
+		'filename' => 'rbac_hook.php',
+		'filepath' => 'third_party/rbac/hooks',
+		'params'   => '',
+);
+//默认开启SESSION
+$hook['pre_system'] = array(
+		'class'    => '',
+		'function' => 'session_start',
+		'filename' => '',
+		'filepath' => '',
+		'params'   => '',
+);
+/* End of file hooks.php */
+/* Location: ./application/config/hooks.php */
