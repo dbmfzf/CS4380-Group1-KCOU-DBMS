242b6c3b1ab19db697a23157a8931270861527a6
072ashin <leiqing126@gmail.com>
2016-04-05 10:30:32 -0500

    Update index.php

--- application/third_party/rbac/controllers/index.php
+++ application/third_party/rbac/controllers/index.php
@@ -10,7 +10,6 @@ class Index extends CI_Controller {
 	
 	function __construct(){
 		//$this -> load -> helpers("rbac_helper");
-		$this-> load -> model("rbac_model");
 		parent::__construct();
 	}
 	/**
@@ -30,7 +29,7 @@ class Index extends CI_Controller {
 	 * 用户登录
 	 */
 	public function login(){
-		
+		$this-> load -> model("rbac_model");
 		$username = $this->input->post('uid');
 		$password = $this->input->post('password');
 		if($username&&$password){
