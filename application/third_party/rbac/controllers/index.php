8a0c6f1996f42414ecc877fff4f88d18caf5e5b1
072ashin <leiqing126@gmail.com>
2016-04-05 10:43:23 -0500

    Update index.php

--- application/third_party/rbac/controllers/index.php
+++ application/third_party/rbac/controllers/index.php
@@ -5,11 +5,9 @@
  * @author		toryzen
  * @link		http://www.toryzen.com
  */
-
 class Index extends CI_Controller {
 	
 	function __construct(){
-		//$this -> load -> helpers("rbac_helper");
 		parent::__construct();
 	}
 	/**
@@ -18,10 +16,10 @@ class Index extends CI_Controller {
 	public function index()
 	{
 		//验证是否登录
-		if(!rbac_conf(array('INFO','uid'))){
-			error_redirct($this->config->item('rbac_auth_gateway'),"Please Login First!");
+		if(!rbac_conf(array('INFO','id'))){
+			error_redirct($this->config->item('rbac_auth_gateway'),"请先登录！");
 		}else{
-			success_redirct($this->config->item('rbac_default_index'),"Success!","1");
+			success_redirct($this->config->item('rbac_default_index'),"您已成功登录,正在跳转请稍候！","1");
 		}
 		
 	}
@@ -29,13 +27,14 @@ class Index extends CI_Controller {
 	 * 用户登录
 	 */
 	public function login(){
-		$this-> load -> model("rbac_model");
+		
+		$this->load->model("rbac_model");
 		$username = $this->input->post('uid');
 		$password = $this->input->post('password');
 		if($username&&$password){
 			$STATUS = $this->rbac_model->check_user($username,md5($password));
 			if($STATUS===TRUE){
-				success_redirct($this->config->item('rbac_default_index'),"Success!");
+				success_redirct($this->config->item('rbac_default_index'),"登录成功！");
 			}else{
 				error_redirct($this->config->item('rbac_auth_gateway'),$STATUS);
 				die();
@@ -50,7 +49,6 @@ class Index extends CI_Controller {
 	 */
 	public function logout(){
 		session_destroy();
-		success_redirct($this->config->item('rbac_auth_gateway'),"Success!",2);
+		success_redirct($this->config->item('rbac_auth_gateway'),"登出成功！",2);
 	}
-
 }
