e6829bbd64a43bdd7593a2627fd3141fb4127fed
072ashin <leiqing126@gmail.com>
2016-04-07 17:08:23 -0500

    Update edit.php

--- application/third_party/rbac/views/manage/member/edit.php
+++ application/third_party/rbac/views/manage/member/edit.php
@@ -37,7 +37,7 @@
       <input value="1" name="status" type="checkbox" <?php if($data["status"]){echo "checked";}?>> 是否启用
     </label>
   </div>
-  <input type="hidden" name="id" value="<?php echo $data['id'];?>">
+  <input type="hidden" name="uid" value="<?php echo $data['uid'];?>">
   <button type="submit" class="btn btn-success">确认修改</button>
   <a class="btn btn-danger" href="<?php echo site_url('manage/member/index'); ?>">取消修改</a>
 </form>
