4660d48a085d1dc3c918ecd4ffc30ffd0bffd83c
072ashin <leiqing126@gmail.com>
2016-04-07 16:34:26 -0500

    Update delete.php

--- application/third_party/rbac/views/manage/role/delete.php
+++ application/third_party/rbac/views/manage/role/delete.php
@@ -2,7 +2,7 @@
 .table td:first-child{width:30%}
 .table td:nth-child(2){width:40%}
 </style>
-<h1>您确定要删除此角色(<?php echo $data["rolename"]; ?>)？</h1>
+<h1>您确定要删除此角色(<?php echo $data["name"]; ?>)？</h1>
 <h4>删除角色将同时删除角色的所有授权，请慎重操作！</h4>
 
 <form method="POST" action="">
