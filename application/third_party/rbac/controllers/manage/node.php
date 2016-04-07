e24cf400aa93620544d03d80a0489958293e7376
072ashin <leiqing126@gmail.com>
2016-04-06 21:43:12 -0500

    Update node.php

--- application/third_party/rbac/controllers/manage/node.php
+++ application/third_party/rbac/controllers/manage/node.php
@@ -21,7 +21,7 @@ class Node extends CI_Controller {
 		if(!empty($node_hidden_array)){
 			$rbac_where = "WHERE ";
 			foreach($node_hidden_array as $node_hidden){
-				$rbac_where.= "dirc != '$node_hidden' AND ";
+				$rbac_where.= "directory != '$node_hidden' AND ";
 			}
 			$rbac_where = substr($rbac_where,0,-4);
 		}
