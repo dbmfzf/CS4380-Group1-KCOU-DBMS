46d2f075f2d053d32f77c7f2b662877398e12b83
072ashin <leiqing126@gmail.com>
2016-04-07 08:20:44 -0500

    Update edit.php

--- application/third_party/rbac/views/manage/menu/edit.php
+++ application/third_party/rbac/views/manage/menu/edit.php
@@ -38,7 +38,7 @@
   </div>
   <input type="hidden" name="level" value="<?php echo $level;?>">
   <input type="hidden" name="id" value="<?php echo $data['id'];?>">
-  <input type="hidden" name="pid" value="<?php echo $p_id;?>">
+  <input type="hidden" name="pid" value="<?php echo $pid;?>">
   <button type="submit" class="btn btn-success">确认修改</button>
   <a class="btn btn-danger" href="<?php echo site_url('manage/menu/index'); ?>">取消修改</a>
 </form>
