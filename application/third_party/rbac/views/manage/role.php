a9eb1308fdc3b165c02032960c6af75f3d0bba5e
072ashin <leiqing126@gmail.com>
2016-04-07 20:29:42 -0500

    Update role.php

--- application/third_party/rbac/views/manage/role.php
+++ application/third_party/rbac/views/manage/role.php
@@ -22,7 +22,7 @@
 						<div class="btn-group  btn-group-xs  pull-right">
 						  <a class="btn btn-default btn-xs" href="%s">编辑角色</a>
 						  <a class="btn btn-info btn-xs" href="%s">赋权节点</a>
-						  <a class="btn btn-danger" href="%s">删除删除</a>
+						  <a class="btn btn-danger" href="%s">删除</a>
 						</div>
 					</td>
 				</tr>',$mb->rid,$mb->name,($mb->status==1?"正常":"停用"),site_url("manage/role/edit/".$mb->rid),site_url("manage/role/action/".$mb->rid),site_url("manage/role/delete/".$mb->rid));
