702b0bafde0fe4792d7e49bbeac0f485fa4b9a78
072ashin <leiqing126@gmail.com>
2016-04-09 15:34:10 -0500

    Update index.php

--- application/third_party/rbac/views/product/index.php
+++ application/third_party/rbac/views/product/index.php
@@ -9,7 +9,7 @@
             <div class="col-6 col-sm-6 col-lg-4">
               <h2>Profile</h2>
               <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-              <p><a class="btn btn-default" href="product/index/edit" role="button">View&Edit &raquo;</a></p>
+              <p><a class="btn btn-default" href="product/index/edit" role="button">View/Edit &raquo;</a></p>
             </div><!--/span-->
             <div class="col-6 col-sm-6 col-lg-4">
               <h2>Music</h2>
