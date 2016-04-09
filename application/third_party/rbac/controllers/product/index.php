4dcfa05ca0586b16407ce9b84598b62535914e99
072ashin <leiqing126@gmail.com>
2016-04-09 15:30:47 -0500

    Update index.php

--- application/third_party/rbac/controllers/product/index.php
+++ application/third_party/rbac/controllers/product/index.php
@@ -4,28 +4,35 @@ class Index extends CI_Controller {
 	
 	function __construct(){
 		parent::__construct();
+		$this->load->database();
 	}
 
-	public function index()
+	public function index($uid)
 	{
-		//取消重写VIEW
+		//cancel rewrite VIEW
 		//$this->view_override = FALSE;
-		$header = array(
-			'header_title'=>'测试系统页面'
-		);
-		$this->load->view("product/index",$header);
-	}
-	
-	public function profile(){
 		
-	}
-	
-	public function music(){
+		$userquery = $this->db->query("SELECT uid,fullname,gender,birthday FROM User WHERE uid = '".$uid."' limit 1");
+		$user_data = $userquery -> row_array();
+		 
+		$rolequery = $this->db->query("SELECT name from Role R, User U where U.rid = R.rid and U.uid = '".$uid."' limit 1");
+		$roledata = $rolequery -> row_array();
 		
-	}
-	
-	public function message(){
+		$loginquery = $this->db->query("SELECT * FROM Login_record WHERE uid = '".$uid."' order by login_id desc limit 1");
+		$login_data = $loginquery -> row_array();
 		
+		
+		$header = 'Home';
+		
+		$data['header'] = $header;
+		$data['uid'] = $user_data['uid'];
+		$data['fullname'] = $user_data['fullname'];
+		$data['gender'] = $user_data['gender'];
+		$data['role'] = $roledata['name'];
+		$data['last_login_time'] = $logindata['date_time'];
+		$data['last_login_ip'] = $logindata['ip'];
+		
+		$this->load->view("product/index",array("data"=>$data),);
 	}
 
 }
