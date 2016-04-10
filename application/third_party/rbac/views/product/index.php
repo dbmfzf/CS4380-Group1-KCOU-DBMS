11a8d289491cd1c24052c306bb80f204588a9b45
072ashin <leiqing126@gmail.com>
2016-04-09 22:08:13 -0500

    Update index.php

--- application/third_party/rbac/views/product/index.php
+++ application/third_party/rbac/views/product/index.php
@@ -3,7 +3,7 @@
           </p>
           <div class="jumbotron">
             <h1>KCOU</h1>
-            <p>this is a kcou management system.</p>
+            <p>This is a kcou management system.</p>
           </div>
           <div class="row">
             <div class="col-6 col-sm-6 col-lg-4">
@@ -13,9 +13,9 @@
               <p><b>Gender:</b> <?php echo $data['gender'];?> </p>
               <p><b>Role:</b> <?php echo $data['role'];?> </p>
               <p><b>Department:</b> <?php echo $data['dept'];?> </p>
-              <p><b>Last login time:</b> </br><?php echo $data['last_login_time'];?> </p>
-              <p><b>Last login ip:</b> </br><?php echo $data['last_login_ip'];?> </p>
-              <p><a class="btn btn-default" href="product/index/edit" role="button">View/Edit &raquo;</a></p>
+              <p><b>Last login time:</b> <?php echo $data['last_login_time'];?> </p>
+              <p><b>Last login ip:</b> <?php echo $data['last_login_ip'];?> </p>
+              <p><a class="btn btn-default" href="product/index/edit" role="button">View &raquo;</a></p>
             </div><!--/span-->
             <div class="col-6 col-sm-6 col-lg-4">
               <h2>Music</h2>
