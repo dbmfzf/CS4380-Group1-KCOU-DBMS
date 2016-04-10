99de3dab98f36713495f0f738ca9a5a936abe618
072ashin <leiqing126@gmail.com>
2016-04-09 22:22:25 -0500

    Update index.php

--- application/third_party/rbac/views/product/index.php
+++ application/third_party/rbac/views/product/index.php
@@ -6,7 +6,7 @@
             <p>This is a kcou management system.</p>
           </div>
           <div class="row">
-            <div class="col-6 col-sm-6 col-lg-4" style="border:1">
+            <div class="col-6 col-sm-6 col-lg-4" style="border:1px solid #F00">
               <h2>Profile</h2>
               <p><b>User ID:</b> <?php echo rbac_conf(array('INFO','uid'));?> </p>
               <p><b>Fullname:</b> <?php echo $data['fullname'];?> </p>
