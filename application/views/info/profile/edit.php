41d40a5cc515c0ba30ab6ca857299e1f8a1bbefc
072ashin <leiqing126@gmail.com>
2016-04-10 16:16:49 -0500

    Create edit.php

---
+++ application/views/info/profile/edit.php
@@ -0,0 +1,51 @@
+<style>
+.tableleft{font-weight:bold;background-color:#F5F5F5;}
+</style>
+<h2>Profile</h2>
+    <table class="table table-bordered table-hover definewidth m10">
+        <tr>
+            <td width = "15%" class="tableleft">User ID</td>
+            <td><p><?php echo rbac_conf(array('INFO','uid'));?></p></td>
+        </tr>     
+        <tr>
+            <td class="tableleft">Full name</td>
+            <td><p><?php echo $data['fullname'];?></p></td>
+        </tr>
+        <tr>
+            <td class="tableleft">Gender</td>
+            <td><p><?php echo $data['gender'];?></p></td>
+        </tr>
+        <tr>
+            <td class="tableleft">Email</td>
+            <td><p><?php echo $data['email'];?></p></td>
+        </tr>
+        <tr>
+            <td class="tableleft">Phone</td>
+            <td><p><?php echo $data['phone'];?></p></td>
+        </tr>
+        <tr>
+            <td class="tableleft">Birthday</td>
+            <td><p><?php echo $data['birth'];?></p></td>
+        </tr>
+        <tr>
+            <td class="tableleft">Role</td>
+            <td><p><?php echo $data['role'];?></p></td>
+        </tr>
+         <tr>
+            <td class="tableleft">Department</td>
+            <td><p><?php echo $data['dept'];?></p></td>
+        </tr>
+         <tr>
+            <td class="tableleft">Last login time</td>
+            <td><p><?php echo $data['last_login_time'];?></p></td>
+        </tr>
+        <tr>
+            <td class="tableleft">Last login ip</td>
+            <td><p><?php echo $data['last_login_ip'];?></p></td>
+        </tr>
+    </table>
+<hr/>
+<a class="btn btn-warning pull-right" href="<?php echo '.site_url("info/profile/reset").';?>">Reset password</a>
+&nbsp;&nbsp;&nbsp;
+<a class="btn btn-success pull-right" href="<?php echo '.site_url("info/profile/edit").';?>">Edit profile</a>
+&nbsp;&nbsp;&nbsp;
