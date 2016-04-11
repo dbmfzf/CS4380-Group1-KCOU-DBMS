e515c9819b932fe7a3643c10c4b24c1fe3356d8b
dbmfzf <dbmfzf@mail.missouri.edu>
2016-04-11 17:17:46 -0500

    Added queries to the music_model

--- application/models/music_model.php
+++ application/models/music_model.php
@@ -4,21 +4,8 @@ class Music_model extends CI_Model {
 
     //Initialize the connection to the database
     public function __construct() {
-        
-        $config['hostname'] = 'us-cdbr-azure-central-a.cloudapp.net';
-        $config['username'] = 'bf22456bcb329e';
-        $config['password'] = '';
-        $config['database'] = 'cs4380-group1';
-        $config['dbdriver'] = 'mysqli';
-        $config['dbprefix'] = '';
-        $config['pconnect'] = FALSE;
-        $config['db_debug'] = TRUE;
-        $config['cache_on'] = FALSE;
-        $config['cachedir'] = '';
-        $config['char_set'] = 'utf8';
-        $config['dbcollat'] = 'utf8_general_ci';
-
-        $this->load->database($config);
+        parent::__construct();
+        $this->load->database();
     }
     
     // Input: param songName which is a string
@@ -27,29 +14,64 @@ class Music_model extends CI_Model {
         $songName = htmlspecialchars($songName);
         
         //SQL string
-        $sql = "SELECT * FROM some_table WHERE id LIKE '%" .  $this->db->escape_like_str($search) . "%'";
+        $sql = "SELECT s.title AS Song_title, ar.name AS Artist, al.title AS Album, s.category AS Genre
+        FROM Song s, Artist_produces_song aps, Artist ar, Releases r,  Album al
+        WHERE s.title LIKE '%" . $this->db->escape_like_str($songName) . 
+        "%' AND aps.sid = s.sid AND ar.artistID = aps.artistID AND ar.artistID = r.artistID AND al.albumID = r.albumID;";
         
         //used to prevent sql injection attacks
-        $this->db->escape($songName);
-        $this->db->escape_like_str($search)
+        //$this->db->escape($songName);
+        //$this->db->escape_like_str($search)
         
-        //
-        $queryObj = $this->db->query($sql, 'id');
+        //gets the return object from the query
+        $queryObj = $this->db->query($sql);
     }
     
     public function searchByArtist($artistName){
-        $artistName = htmlspecialchars($songName);
+        $artistName = htmlspecialchars($artistName);
+        $sql = "SELECT s.title AS Song_title, ar.name AS Artist, al.title AS Album, s.category AS Genre
+        FROM Song s, Artist_produces_song aps, Artist ar, Releases r,  Album al
+        WHERE ar.name LIKE '%" . $this->db->escape_like_str($artistName) . 
+        "%' AND aps.sid = s.sid AND ar.artistID = aps.artistID AND ar.artistID = r.artistID AND al.albumID = r.albumID;";
         
+        $queryObj = $this->db->query($sql);
     }
     
     public function searchByAlbum($albumName){
-        $albumName = htmlspecialchars($albumname);
+        $albumName = htmlspecialchars($albumName);
         
+        $sql = "SELECT s.title AS Song_title, ar.name AS Artist, al.title AS Album, s.category AS Genre
+        FROM Song s, Artist_produces_song aps, Artist ar, Releases r,  Album al
+        WHERE al.title LIKE '%" . $this->db->escape_like_str($albumName) . 
+        "%' AND aps.sid = s.sid AND ar.artistID = aps.artistID AND ar.artistID = r.artistID AND al.albumID = r.albumID;";
+        
+        $queryObj = $this->db->query($sql);
+
     }
     
     //Searches for album name, artist name, and song name all at once
     public function genericSearch($searchString){
         $searchString = htmlspecialchars($searchString);
+        
+        $sql = "SELECT s.title AS Song_title, ar.name AS Artist, al.title AS Album, s.category AS Genre
+        FROM Song s, Artist_produces_song aps, Artist ar, Releases r,  Album al
+        WHERE (al.title LIKE '%" . $this->db->escape_like_str($searchString) . 
+        "%' OR ar.name LIKE '%" . $this->db->escape_like_str($searchString) .
+        "%' OR s.title LIKE '%" . $this->db->escape_like_str($searchString) . "%')
+         AND aps.sid = s.sid AND ar.artistID = aps.artistID AND ar.artistID = r.artistID AND al.albumID = r.albumID;";
+        
+        $queryObj = $this->db->query($sql);
+    }
+    //Input: a genre of music that is exact
+    public function searchByGenre($genre){
+        $genre = htmlspecialchars($genre);
+        
+        $sql = "SELECT s.title AS Song_title, ar.name AS Artist, al.title AS Album, s.category AS Genre
+        FROM Song s, Artist_produces_song aps, Artist ar, Releases r,  Album al
+        WHERE s.category = '" . $this->db->escape($genre) 
+        . "' AND aps.sid = s.sid AND ar.artistID = aps.artistID AND ar.artistID = r.artistID AND al.albumID = r.albumID;";
+        
+        $queryObj = $this->db->query($sql);
     }
 
 }
