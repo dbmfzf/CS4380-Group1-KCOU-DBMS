1c1c350e8a2d042d4ff78a4b171b4617acd8a79f
072ashin <leiqing126@gmail.com>
2016-04-10 21:16:53 -0500

    Update add.php

--- application/views/info/user/add.php
+++ application/views/info/user/add.php
@@ -1,21 +1,33 @@
-<h1>新增用户</h1>
+<h1>Add new user</h1>
 <form role="form" action="" method="post">
   <div class="form-group">
-    <label>用户ID</label>
-    <input name="uid" type="text" class="form-control"  placeholder="在此输入帐号" value="">
+    <label>User serID</label>
+    <input name="uid" type="text" class="form-control"  placeholder="Please input user ID here" value="">
   </div>
   <div class="form-group">
-    <label>姓名</label>
-    <input name="fullname" type="text" class="form-control" placeholder="在此输入昵称" value="">
+    <label>Full name</label>
+    <input name="fullname" type="text" class="form-control" placeholder="Please input the full name here" value="">
+  </div>
+  <div class="form-group">
+    <label>Gender</label>
+    <label><input name="gender" type="radio" class="form-control" value="Male">Male</label>
+    <label><input name="gender" type="radio" class="form-control" value="Female">Female</label>
   </div>
   <div class="form-group">
     <label>Email</label>
-    <input name="email" type="email" class="form-control" placeholder="在此输入Email" value="">
+    <input name="email" type="email" class="form-control" placeholder="Please input the email here" value="">
+  </div>
+  <div class="forn-group">
+    <label>Phone</label>
+    <input name="phone" type="number" class="form-control" placeholder="Please input the phone number here" value="">
   </div>
   <div class="form-group">
-    <label>角色</label>
+    <label>Birthday</label>
+    <input name="birth" type="date" class="form-control" placeholder="Please input or select the birthday here" value="">
+  </div>
+  <div class="form-group">
+    <label>Role</label>
     <select name="role"  class="form-control" >
-    	<option value=''>暂无角色</option>
     	<?php 
     		foreach($role_data as $vo){
     			echo "<option value='{$vo->rid}'>{$vo->name}</option>";
@@ -24,18 +36,28 @@
     </select>
   </div>
   <div class="form-group">
-    <label>新密码</label>
-    <input name="password" type="password" class="form-control" placeholder="在此输入密码" value="">
+    <label>Department</label>
+    <select name="role"  class="form-control" >
+    	<?php 
+    		foreach($dept_data as $vo){
+    			echo "<option value='{$vo->did}'>{$vo->name}</option>";
+    		}
+    	?>
+    </select>
   </div>
   <div class="form-group">
-    <label>确认密码</label>
-    <input name="password2" type="password" class="form-control" placeholder="在此确认密码" value="">
+    <label>New password</label>
+    <input name="password" type="password" class="form-control" placeholder="Please input the new password here" value="">
+  </div>
+  <div class="form-group">
+    <label>Repeat the password</label>
+    <input name="password2" type="password" class="form-control" placeholder="Please repeat the new password here" value="">
   </div>
   <div class="checkbox">
     <label>
-      <input value="1" name="status" type="checkbox" checked > 是否启用
+      <input value="1" name="status" type="checkbox" checked > Enable?
     </label>
   </div>
-  <button type="submit" class="btn btn-success">确认新增</button>
-  <a class="btn btn-danger" href="<?php echo site_url('info/user/index'); ?>">取消修改</a>
+  <button type="submit" class="btn btn-success">Save</button>
+  <a class="btn btn-danger" href="<?php echo site_url('info/user/index'); ?>">Cancel</a>
 </form>
