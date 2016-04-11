88619e3793ae543eab1a451aeadc1046fc8c5efc
072ashin <leiqing126@gmail.com>
2016-04-10 21:53:14 -0500

    Update user.php

--- application/views/info/user.php
+++ application/views/info/user.php
@@ -29,6 +29,7 @@
 					<td>%s</td>
 					<td>%s</td>
 					<td>%s</td>
+					<td>%s</td>
 					<td>
 						<div class="btn-group  btn-group-xs">
 						  <a class="btn btn-default btn-xs" href="%s">Edit</a>
