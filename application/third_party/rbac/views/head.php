2fd0118be3aaf28031f6572d8b59f790b7cd5823
072ashin <leiqing126@gmail.com>
2016-04-12 18:23:59 -0500

    Update head.php

--- application/third_party/rbac/views/head.php
+++ application/third_party/rbac/views/head.php
@@ -7,7 +7,7 @@
 		<link rel="stylesheet" href="<?php echo base_url();?>static/bootstrap/css/bootstrap.min.css">
 		<link href="<?php echo base_url();?>static/offcanvas.css" rel="stylesheet">
         <title>KCOU management system</title>
-        <script src="<?php echo base_url();?>static/jquery.1102.min.js"></script>
+        <script src="<?php echo base_url();?>static/jquery.js"></script>
 	<script src="<?php echo base_url();?>static/bootstrap/js/bootstrap.min.js"></script>
         <script src="<?php echo base_url();?>static/bootstrap/js/respond.min.js"></script>
 	<script src="<?php echo base_url();?>static/highcharts.js"></script>
