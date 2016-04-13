84f699d789598beb12d72c68b66822e2cd83ea4a
072ashin <leiqing126@gmail.com>
2016-04-13 16:34:01 -0500

    Update analytics.php

--- application/third_party/rbac/controllers/manage/analytics.php
+++ application/third_party/rbac/controllers/manage/analytics.php
@@ -12,7 +12,7 @@ class Analytics extends CI_Controller {
 		$user_dept_query = $this->db->query("SELECT D.name as dept_name, count(*) as user_num FROM User U, Department D, Belongs_to B WHERE B.uid = U.uid AND B.did = D.did GROUP BY D.did");
 		$user_dept_data = $user_dept_query->result();
 		
-		$user_gender_query = $this->db->query("SELECT count(*) as gender_cnt FROM USER GROUP BY gender");
+		$user_gender_query = $this->db->query("SELECT gender count(*) as user_num FROM USER GROUP BY gender");
 		$user_gender_data = $user_gender_query->result();
 		
 		$user_role_query = $this->db->query("SELECT count(*) as user_num, R.name as role_name FROM User U, Role R WHERE R.rid = U.rid GROUP BY R.rid");
