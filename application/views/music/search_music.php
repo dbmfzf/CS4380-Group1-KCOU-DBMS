8a8d2d05d9160b6a0ccb53a8c06b85e50d149684
dbmfzf <dbmfzf@mail.missouri.edu>
2016-04-12 20:21:14 -0500

    linking css and javascript

--- application/views/music/search_music.php
+++ application/views/music/search_music.php
@@ -1,36 +1,71 @@
-<head>
-    <?php echo link_tag('css/style.css'); ?>
-    <?php echo link_tag('js/ajax_music_search.js'); ?>
-   <!-- <script src="js/ajax_music_search.js"></script>
-    <link rel="stylesheet" href="css/style.css">-->
-</head>
-<body>
-    <div class="well">
-        <div class="well well-lg" id="searchBar">
-            <form role="form" class="form-inline" id="searchForm">
-                 <span class="form-group">
-                    <div class="collapse in advancedSearch">
-                        <input type="text" class="form-control" id="genericSearch" placeholder="Enter song name, artist name, or album name here">
-                    </div>
-                    <div class="collapse advancedSearch" id="advancedOptions">
-                        <input type="text" class="form-control" id="songSearch" placeholder="Song name">
-                        <input type="text" class="form-control" id="artistSearch" placeholder="Artist name">
-                        <input type="text" class="form-control" id="albumSearch" placeholder="Album name">
-                        <input type="text" class="form-control" id="genreSearch" placeholder="Genre">
-                    </div>
-                </span>
-                <span class="form-group btn-group">
-                    <button data-toggle="collapse" data-target=".advancedSearch" class="form-control">Advanced Search...</button>
-                    <button type="button" class="btn btn-default" onclick="searchMusic('generic')"><span class="glyphicon glyphicon-search"></span>  Search</button>
-                </span>
-            </form>
-        </div>
-        <div id="resultArea">
-            <?php
-            for($i = 0; $i < 10; $i++){
-                echo "<div class=\"songContent well\"></div>";
-            }
-        ?>
-        </div>
+<script>
+    $(document).ready(function(){
+        var searchWidth = $("#advancedOptions").innerWidth();
+        $("#genericSearch").width(searchWidth);
+    });
+
+
+
+    function searchMusic(searchType){
+        switch(searchType){
+            case 'generic':
+                $.getJSON("../../../controllers/music/search_music/genericSearchHandler",
+                    $("#genericSearch").val(), function(){
+                        //parse the json
+                    });
+                break;
+            case 'advanced':
+                break;
+            default:
+                break;
+        }
+}
+</script>
+<style>
+    #genericSearch{
+        width: 350px;
+    }
+    .songContent {
+        margin: 0px;
+        height: 150px;
+    }
+    .songContent:nth-child(even) {
+        background: #DDD;
+    }
+    .songContent:nth-child(odd) {
+        background: #FFF;
+    }
+    #searchBar {
+        background-color: #AAA;
+    }
+
+</style>
+<div class="well">
+    <div class="well well-lg" id="searchBar">
+        <form role="form" class="form-inline" id="searchForm">
+             <span class="form-group">
+                <div class="collapse in advancedSearch">
+                    <input type="text" class="form-control" id="genericSearch" placeholder="Enter song name, artist name, or album name here">
+                </div>
+                <div class="collapse advancedSearch" id="advancedOptions">
+                    <input type="text" class="form-control" id="songSearch" placeholder="Song name">
+                    <input type="text" class="form-control" id="artistSearch" placeholder="Artist name">
+                    <input type="text" class="form-control" id="albumSearch" placeholder="Album name">
+                    <input type="text" class="form-control" id="genreSearch" placeholder="Genre">
+                </div>
+            </span>
+            <span class="form-group btn-group">
+                <button data-toggle="collapse" data-target=".advancedSearch" class="form-control">Advanced Search...</button>
+                <button type="button" class="btn btn-default" onclick="searchMusic('generic')"><span class="glyphicon glyphicon-search"></span>  Search</button>
+            </span>
+        </form>
     </div>
-</body>
+    <div id="resultArea">
+        <?php
+        for($i = 0; $i < 10; $i++){
+            echo "<div class=\"songContent well\"></div>";
+        }
+    ?>
+    </div>
+</div>
+
