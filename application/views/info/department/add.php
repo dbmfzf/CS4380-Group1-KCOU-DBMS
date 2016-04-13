f96d3046de1ed82b1188252f5834685b01b4c6f6
Keevey <keeveysong@gmail.com>
2016-04-13 15:16:28 -0600

    DEPARTMENT

--- application/views/info/department/add.php
+++ application/views/info/department/add.php
@@ -4,11 +4,10 @@
 <h1>Add new department</h1>
 <form action="" method="post"> 
     <table class="table table-bordered table-hover definewidth m10">
-        <tr>
-            <td width = "15%" class="tableleft">User ID</td>
-            <td><input name="dname" type="text" class="form-control" placeholder="Please input department name here"></td>
+      <tr>
+            <td class="tableleft">Department</td>
+            <td><input type="text" name="dname"</td>
         </tr>
-        
         <tr>
             <td class="tableleft">Role</td>
             <td>
@@ -21,18 +20,7 @@
                 </select>
             </td>
         </tr>
-        <tr>
-            <td class="tableleft">Department</td>
-            <td>
-                <select name="dname" id="dept" class="form-control" >
-                  	<?php 
-                  		foreach($dept_data as $vo){
-                  			echo "<option value='{$vo->did}'>{$vo->name}</option>";
-                  		}
-                  	?>
-                </select>
-            </td>
-        </tr>
+        
     </table>
     <button type="submit" class="btn btn-success">Save</button> 
      <a class="btn btn-danger" href="<?php echo site_url('info/department/index'); ?>">Cancel</a> 
