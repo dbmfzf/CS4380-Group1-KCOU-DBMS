71d894fbb5f2e5ca3abc2027300ed98325d2f3cc
Keevey <keeveysong@gmail.com>
2016-04-13 14:38:45 -0600

    Department

--- application/controllers/info/department.php
+++ application/controllers/info/department.php
@@ -11,23 +11,18 @@ class Department extends CI_Controller {
 	 */
 	public function index($page=1)
 	{
-		$did = rbac_conf(array('INFO','did'));
 		$query = $this->db->query("SELECT COUNT(1) as cnt FROM department");
 		$cnt_data = $query->row_array();
 		//page
 		$this->load->library('pagination');
-		$config['base_url'] = site_url("info/user/index");
+		$config['base_url'] = site_url("info/department/index");
 		$config['total_rows'] = $cnt_data['cnt'];
 		$config['per_page']   = 35;
 		$config['uri_segment']= '4';
 		$config['use_page_numbers'] = TRUE;
 		$this->pagination->initialize($config);
 		$role_dept_query = $this->db->query("Select d.did, d.name as dname, d.rid, r.name as rname from department d, role r WHERE r.rid = d.rid and d.did = '{$did}'");
-		$role_dept_data = $role_dept_query->row_array();
-		$rolename = $role_dept_data['rname'];
-		if(($rolename="Manager")or($rolename="Volunteer")){$where="";} else{$where = "AND D.did ='".$did."'";}
-		$query = $this->db->query("SELECT d.name as dname, r.name as rname FROM department d, role r WHERE r.rid = d.rid  ".$where." LIMIT ".(($page-1)*$config['per_page']).",".$config['per_page']."");
-		$data = $query->result();
+		$data = $role_dept_query->row_array();
 		$this->load->view("info/department",array("data"=>$data));
 	}
 	/**
@@ -44,32 +39,30 @@ class Department extends CI_Controller {
 		$data['did'] = $department_data['did'];
 		$data['name'] = $department_data['dname'];
 		
-		$data['rolename'] = $current_role['rname'];
+		$data['rname'] = $current_role['rname'];
 		$data['rid'] = $current_role['rid'];
 
 
 		
 		if($data){
 			if($this->input->post()){
-				$name = $this->input->post("dname");
-				$rolename = $this->input->post("rname");
+				$dname = $this->input->post("dname");
+				$rname = $this->input->post("rname");
 				
-				$role_dept_query = $this->db->query("SELECT rid from role WHERE rname = ".$rolename."");
+				$role_dept_query = $this->db->query("SELECT rid from role WHERE rname = ".$rname."");
 				$role_dept_data = $role_dept_query->row_array();
 				$rid = $role_dept_data['rid'];
 
-				if($uid!=""){
-					if($did&&$name&&$rolename){
-						$rsql = "UPDATE department, set name='{$rolename}' name='{$rolename}' WHERE uid = '{$uid}'";
+				if($did!=""){
+					if($did&&$name&&$rname){
+						$rsql = "UPDATE department, set name='{$dname}' , rid='{$rid}' WHERE did = '{$did}'";
 						$this->db->query($sql);
-						$sql = "UPDATE Belongs_to set did = $dept WHERE uid = '{$uid}'";
-						$this->db->query($dsql);
 						success_redirct("info/department/index","Edit successful!");
 					}else{
 						error_redirct("","The department's information is not complete!");
 					}
 				}else{
-					error_redirct("","No user is found!");
+					error_redirct("","No department is found!");
 				}
 			}
 			$this->load->view("info/department/edit",array("data"=>$data,"role_data"=>$role_data,"dept_data"=>$dept_data));
@@ -82,44 +75,35 @@ class Department extends CI_Controller {
 	 */
 	public function add(){
 		
-		$role_query = $this->db->query("SELECT rid,name FROM role order by rid desc");
+		$role_query = $this->db->query("SELECT rid,name as rname FROM role order by rid desc");
 		$role_data = $role_query->result();
 		
-		$dept_query = $this->db->query("SELECT did,name FROM department order by did desc");
+		$dept_query = $this->db->query("SELECT did,name as dname FROM department order by did desc");
 		$dept_data = $dept_query->result();
 		
 		if($this->input->post()){
 			$dname = $this->input->post("dname");
 			
-			$rolename = $this->input->post("rolename");
-			$role_dept_query = $this->db->query("SELECT d.did from department d, role r WHERE d.rid = r.rid and r.name = ".$rolename."");
+			$rname = $this->input->post("rname");
+			$role_dept_query = $this->db->query("SELECT rid from role r WHERE name = ".$rname."");
 			$role_dept_data = $role_dept_query->row_array();
-			$did = $role_dept_data['did'];
+			$rid = $role_dept_data['rid'];
 		
 			if($dname&&$rolename){
 				$query = $this->db->query("SELECT * FROM department WHERE did = '".$did."'");
 				$data = $query->row_array();
 				if(!$data){
-					$query = $this->db->query("SELECT * FROM User WHERE email = '".$email."'");
-						$data = $query->row_array();
-						if(!$data){
-							if(!$did){$newdept = $did;}else{$newdept = $dept;}
-							if(!$status){$newstat = "0";}else{$newstat = "1";}
-							$sql = "INSERT INTO User (uid,fullname,gender,email,phone,birth,password,rid,status) values('{$uid}','{$fullname}','{$gender}','{$email}','{$phone}','{$birth}','{$password2}','{$role}', '{$newstat}')";
-							$this->db->query($sql);
-							$dsql = "INSERT INTO Belongs_to(uid,did) values('{$uid}','{$newdept}')";
-							$this->db->query($dsql);
-							success_redirct("info/department/index","Add successful!");
+					$sql = "INSERT INTO department (name,rid) values('{$dname}','{$rid}')";
+					$this->db->query($sql);
+					success_redirct("info/department/index","Add successful!");
 					}else{
 						error_redirct("","The department already exists!");
-					}
-					
+					}		
 				}else{
-					error_redirct("","The user's information is not complete!");
+					error_redirct("","The department's information is not complete!");
 			}
 		}
 		$this->load->view("info/department/add",array("role_data"=>$role_data,"dept_data"=>$dept_data));
-	}
 	}
 	
 	/**
@@ -141,7 +125,7 @@ class Department extends CI_Controller {
 			}
 			$this->load->view("info/department/delete",array("data"=>$data));
 		}else{
-			error_redirct("info/department/index","No user is found!");
+			error_redirct("info/department/index","No department is found!");
 		}
 	}
 }
--- application/views/info/news/news_submission.php
+++ application/views/info/news/news_submission.php
@@ -5,8 +5,8 @@
 <title>kindeditor</title>
 
 <!-- 在这里引入css文件 -->
-<link rel="stylesheet" href="kindeditor/themes/default/default.css" />
-<link rel="stylesheet" href="kindeditor/plugins/code/prettify.css" />
+<link rel="stylesheet" href="<?php echo base_url();?>kindeditor/themes/default/default.css" />
+<link rel="stylesheet" href="<?php echo base_url();?>kindeditor/plugins/code/prettify.css" />
 </head>
 <body>
     <form action="Diary.php" name="myform" method="post">
@@ -22,9 +22,9 @@
 <script>
    KindEditor.ready(function(K) {//kindeditor控件调用
        var editor1=K.create('textarea[name="content"]',{//name=form中textarea的name属性
-           cssPath : '../../kindeditor/plugins/code/prettify.css',
-           uploadJson : '../../kindeditor/php/upload_json.php',
-           fileManagerJson : '../../kindeditor/php/file_manager_json.php',
+           cssPath : '<?php echo base_url();?>kindeditor/plugins/code/prettify.css',
+           uploadJson : <?php echo base_url();?>kindeditor/php/upload_json.php',
+           fileManagerJson : '<?php echo base_url();?>kindeditor/php/file_manager_json.php',
            allowFileManager : true,
            afterCreate : function() {
                var self = this;
