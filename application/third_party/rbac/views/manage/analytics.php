d6f7c25b37277c1319271191de381f50f71d8350
072ashin <leiqing126@gmail.com>
2016-04-13 13:52:15 -0500

    Update analytics.php

--- application/third_party/rbac/views/manage/analytics.php
+++ application/third_party/rbac/views/manage/analytics.php
@@ -1,22 +1,19 @@
+<h1>Distribution of users</h1>
 <ul id="myTab" class="nav nav-tabs">
-   <li class="active">
-      <a href="#home" data-toggle="tab">
-         W3Cschool Home
-      </a>
+   <li class="active"><a href="#home" data-toggle="tab">By department</a>
    </li>
-   <li><a href="#ios" data-toggle="tab">iOS</a></li>
-   <li><a href="#java" data-toggle="tab">JAVA</a>
-   </li>
+   <li><a href="#second" data-toggle="tab">By gender</a></li>
+   <li><a href="#third" data-toggle="tab">By role</a></li>
 </ul>
 <div id="myTabContent" class="tab-content">
    <div class="tab-pane fade in active" id="home">
       <p>W3Cschoool菜鸟教程是一个提供最新的web技术站点，本站免费提供了建站相关的技术文档，帮助广大web技术爱好者快速入门并建立自己的网站。菜鸟先飞早入行——学的不仅是技术，更是梦想。</p>
    </div>
-   <div class="tab-pane fade" id="ios">
+   <div class="tab-pane fade" id="second">
       <p>iOS 是一个由苹果公司开发和发布的手机操作系统。最初是于 2007 年首次发布 iPhone、iPod Touch 和 Apple 
       TV。iOS 派生自 OS X，它们共享 Darwin 基础。OS X 操作系统是用在苹果电脑上，iOS 是苹果的移动版本。</p>
    </div>
-   <div class="tab-pane fade" id="JAVA">
+   <div class="tab-pane fade" id="third">
       <p>jMeter 是一款开源的测试软件。它是 100% 纯 Java 应用程序，用于负载和性能测试。</p>
    </div>
 </div>
