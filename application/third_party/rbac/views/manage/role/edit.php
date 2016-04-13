5840243451c734613fbc3582a181dc463e5e93b9
072ashin <leiqing126@gmail.com>
2016-04-12 22:20:36 -0500

    Update edit.php

--- application/third_party/rbac/views/manage/role/edit.php
+++ application/third_party/rbac/views/manage/role/edit.php
@@ -1,15 +1,15 @@
-<h1>角色编辑</h1>
+<h1>Edit role</h1>
 <form role="form" action="" method="post">
   <div class="form-group">
-    <label>角色名</label>
+    <label>Role name</label>
     <input name="rolename" type="text" class="form-control" value="<?php echo $data['name']; ?>">
   </div>
   <div class="checkbox">
     <label>
-      <input value="1" name="status" type="checkbox" <?php if($data["status"]){echo "checked";}?>> 是否启用
+      <input value="1" name="status" type="checkbox" <?php if($data["status"]){echo "checked";}?>> Enable?
     </label>
   </div>
   <input type="hidden" name="rid" value="<?php echo $data['rid'];?>">
-  <button type="submit" class="btn btn-success">确认修改</button>
-  <a class="btn btn-danger" href="<?php echo site_url('manage/role/index'); ?>">取消修改</a>
+  <button type="submit" class="btn btn-success">Save</button>
+  <a class="btn btn-danger" href="<?php echo site_url('manage/role/index'); ?>">Cancel</a>
 </form>
