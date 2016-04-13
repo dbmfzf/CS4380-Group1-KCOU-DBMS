864f8a0eef4201c2f281f0d3198666e29e385e67
072ashin <leiqing126@gmail.com>
2016-04-13 13:40:46 -0500

    Update analytics.php

--- application/third_party/rbac/views/manage/analytics.php
+++ application/third_party/rbac/views/manage/analytics.php
@@ -1,39 +1,22 @@
-<style>
-.table td:first-child{width:10%}
-.table td:nth-child(2){width:20%}
-</style>
-<table class="table  table-bordered well">
-	<thead>
-          <tr>
-            <th>ID</th>
-            <th>姓名</th>
-            <th>Email</th>
-            <th>角色</th>
-            <th>状态</th>
-            <th>操作</th>
-          </tr>
-        </thead>
-   <tbody>
-	<?php 
-	foreach($data as $mb){
-		printf('<tr>
-					<td>%s</td>
-					<td>%s</td>
-					<td>%s</td>
-					<td>%s</td>
-					<td>%s</td>
-					<td>
-						<div class="btn-group  btn-group-xs">
-						  <a class="btn btn-default btn-xs" href="%s">编辑</a>
-						  <a class="btn btn-danger" href="%s">删除</a>
-						</div>
-					</td>
-				</tr>',$mb->uid,$mb->fullname,$mb->email,($mb->name?$mb->name:"暂无角色"),($mb->status==1?"正常":"停用"),site_url("manage/member/edit/".$mb->uid),site_url("manage/member/delete/".$mb->uid));
-	}
-	?>
-  </tbody>
-</table>
-<hr/>
-
-<?php echo '<a class="btn btn-success pull-right" href="'.site_url("manage/member/add").'">新增用户</a>'; ?>
-<?php echo $this->pagination->create_links(); ?>
+<ul id="myTab" class="nav nav-tabs">
+   <li class="active">
+      <a href="#home" data-toggle="tab">
+         W3Cschool Home
+      </a>
+   </li>
+   <li><a href="#ios" data-toggle="tab">iOS</a></li>
+   <li><a href="#java" data-toggle="tab">JAVA</a>
+   </li>
+</ul>
+<div id="myTabContent" class="tab-content">
+   <div class="tab-pane fade in active" id="home">
+      <p>W3Cschoool菜鸟教程是一个提供最新的web技术站点，本站免费提供了建站相关的技术文档，帮助广大web技术爱好者快速入门并建立自己的网站。菜鸟先飞早入行——学的不仅是技术，更是梦想。</p>
+   </div>
+   <div class="tab-pane fade" id="ios">
+      <p>iOS 是一个由苹果公司开发和发布的手机操作系统。最初是于 2007 年首次发布 iPhone、iPod Touch 和 Apple 
+      TV。iOS 派生自 OS X，它们共享 Darwin 基础。OS X 操作系统是用在苹果电脑上，iOS 是苹果的移动版本。</p>
+   </div>
+   <div class="tab-pane fade" id="JAVA">
+      <p>jMeter 是一款开源的测试软件。它是 100% 纯 Java 应用程序，用于负载和性能测试。</p>
+   </div>
+</div>
