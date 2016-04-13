75292272c2fcd198f89e892fbaa78a9d49e9c2c9
dbmfzf <dbmfzf@mail.missouri.edu>
2016-04-12 20:42:42 -0500

    fixing the search bar

--- application/views/music/search_music.php
+++ application/views/music/search_music.php
@@ -44,18 +44,18 @@
     <div class="well well-lg" id="searchBar">
         <form role="form" class="form-inline" id="searchForm">
              <span class="form-group">
-                <div class="collapse in advancedSearch">
+                <span class="collapse in advancedSearch">
                     <input type="text" class="form-control" id="genericSearch" placeholder="Enter song name, artist name, or album name here">
-                </div>
-                <div class="collapse advancedSearch" id="advancedOptions">
+                </span>
+                <span class="collapse advancedSearch" id="advancedOptions">
                     <input type="text" class="form-control" id="songSearch" placeholder="Song name">
                     <input type="text" class="form-control" id="artistSearch" placeholder="Artist name">
                     <input type="text" class="form-control" id="albumSearch" placeholder="Album name">
                     <input type="text" class="form-control" id="genreSearch" placeholder="Genre">
-                </div>
+                </span>
             </span>
-            <span class="form-group btn-group">
-                <button href="#" data-toggle="collapse" data-target=".advancedSearch" class="form-control">Advanced Search...</button>
+            <span class="btn-group">
+                <button data-toggle="collapse" data-target=".advancedSearch" class="form-control">Advanced Search...</button>
                 <button type="button" class="btn btn-default" onclick="searchMusic('generic')"><span class="glyphicon glyphicon-search"></span>  Search</button>
             </span>
         </form>
