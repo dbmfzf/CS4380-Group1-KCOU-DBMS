aaeb061cd42411e05a970a0b932df66cd62dc8bf
072ashin <leiqing126@gmail.com>
2016-04-12 22:19:58 -0500

    Update delete.php

--- application/third_party/rbac/views/manage/role/delete.php
+++ application/third_party/rbac/views/manage/role/delete.php
@@ -2,11 +2,11 @@
 .table td:first-child{width:30%}
 .table td:nth-child(2){width:40%}
 </style>
-<h1>您确定要删除此角色(<?php echo $data["name"]; ?>)？</h1>
-<h4>删除角色将同时删除角色的所有授权，请慎重操作！</h4>
+<h1>Are you sure to delete this role(<?php echo $data["name"]; ?>)?</h1>
+<h4>All the role's previliges will be deleted when you delete the role, please think twice!</h4>
 
 <form method="POST" action="">
 	<input type="hidden" name="verfiy" value="1" >
-	<input class="btn btn-success"  type="submit" value="确定删除">
-	<a class="btn btn-danger" href="<?php echo site_url('manage/role/index'); ?>">取消修改</a>
+	<input class="btn btn-success"  type="submit" value="OK">
+	<a class="btn btn-danger" href="<?php echo site_url('manage/role/index'); ?>">Cancel</a>
 </form>
