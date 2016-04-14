adbc2dc21c18fd4ee07bed14bba1e7d4587171b1
072ashin <leiqing126@gmail.com>
2016-04-14 12:30:34 -0500

    Update analytics.php

--- application/third_party/rbac/views/manage/analytics.php
+++ application/third_party/rbac/views/manage/analytics.php
@@ -1,5 +1,47 @@
+<script>
+<?php
+        foreach($user_gender_data as $key){
+		 $gender[]=$key['gender'];
+		 $gender_num[]=intval($key['gender_num']);
+	}
+	$gender_data = array(array("name"=>$gender,"data"=>$gender_num));
+	$gender_data = json_encode($gender_data);
+	echo $gender_data;
+?>
+$(function () {
+        // Build the chart
+        $('#gender').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: 'Gender'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false
+                    },
+                    showInLegend: true
+                }
+            },
+            series: [{
+                name: 'Brands',
+                colorByPoint: true,
+                data: <?php echo $gender_data; ?>
+            }]
+        });
+});
+</script>
 <h1>Distribution of users</h1>
-
 <ul id="myTab" class="nav nav-tabs">
    <li class="active"><a href="#home" data-toggle="tab">By department</a>
    </li>
@@ -14,6 +56,6 @@
       <div id="gender" style="min-width: 310px; height: 600px; max-width: 800px; margin: 0 auto; padding-top:5%"></div>
    </div>
    <div class="tab-pane fade" id="third">
-      <div id="role" style="min-width: 310px; height: 600px; max-width: 800px; margin: 0 auto; padding-top:5%"></div>
+      <div id="role" style="min-width: 310px; height: 600px; max-width: 800px; margin: 0 auto; padding-top:5%"><?php echo $gender_data; ?></div>
    </div>
 </div>
