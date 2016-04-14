970c054fc0884cbd11562937aa5ed416624344de
dbmfzf <dbmfzf@mail.missouri.edu>
2016-04-14 16:30:18 -0500

    fixing search bar

--- application/views/music/search_music.php
+++ application/views/music/search_music.php
@@ -44,35 +44,42 @@
     #searchBar {
         background-color: #AAA;
     }
-
+    #toggleSearch {
+        margin: 5px 0;
+    }
+    /*
+    if I can't fix the position of the search button then this will do it just as well
+    works for both search buttons
+    {margin-top: 25px}
+    */
 </style>
 <div class="well">
     <div class="well well-lg" id="searchBar">
         <form role="form" class="form-inline collapse in" id="genericSearchForm">
                 <div class="form-group">
-                  <label for="genericSearch">Search for: </label>
+                  <label for="genericSearch">Search for:</label>
                    <input type="text" class="form-control" id="genericSearch" placeholder="Enter song name, artist name, or album name here"> 
                 </div>
                 <div class="form-group">
                     <button type="button" class="btn btn-primary" onclick="searchMusic('generic')"><span class="glyphicon glyphicon-search"></span>  Search</button>
                 </div>
         </form>
-        <button type="button" data-toggle="collapse" data-target=".form-inline" class="btn btn-primary" id="toggleSearch" onclick="toggleOptions()">Advanced Search <span class="glyphicon glyphicon-triangle-bottom"></span> </button>
+        <button type="button" data-toggle="collapse" data-target=".form-inline" class="btn btn-primary" id="toggleSearch" onclick="toggleOptions()">Advanced Search<span class="glyphicon glyphicon-triangle-bottom"></span> </button>
         <form role="form" class="form-inline collapse" id="advancedSearchForm">
                 <div class="form-group">
-                    <label for="songSearch">Song: </label>
+                    <label for="songSearch">Song:</label>
                     <input type="text" class="form-control" id="songSearch" placeholder="Song name">
                 </div>
                 <div class="form-group">
-                    <label for="artistSearch">Artist: </label>
+                    <label for="artistSearch">Artist:</label>
                     <input type="text" class="form-control" id="artistSearch" placeholder="Artist name">
                 </div>
                 <div class="form-group">
-                    <label for="albumSearch">Album: </label>
+                    <label for="albumSearch">Album:</label>
                     <input type="text" class="form-control" id="albumSearch" placeholder="Album name">
                 </div>
                 <div class="form-group">
-                    <label for="genreSearch">Genre: </label>
+                    <label for="genreSearch">Genre:</label>
                     <input type="text" class="form-control" id="genreSearch" placeholder="Genre">
                 </div>
                 <div class="form-group">
