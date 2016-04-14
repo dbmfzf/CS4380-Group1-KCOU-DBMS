a2a63c4ed1caf64549c5c8c198fcc1870ea9cb25
dbmfzf <dbmfzf@mail.missouri.edu>
2016-04-14 09:57:47 -0500

    fixing search bar

--- application/views/music/search_music.php
+++ application/views/music/search_music.php
@@ -41,10 +41,23 @@
     <div class="well well-lg" id="searchBar">
         <form role="form" class="form-inline" id="searchForm">
                 <input type="text" class="form-control" id="genericSearch" placeholder="Enter song name, artist name, or album name here">
-                <input type="text" class="form-control" id="songSearch" placeholder="Song name">
-                <input type="text" class="form-control" id="artistSearch" placeholder="Artist name">
-                <input type="text" class="form-control" id="albumSearch" placeholder="Album name">
-                <input type="text" class="form-control" id="genreSearch" placeholder="Genre">
+                <div class="form-group">
+                    <label for="songSearch">Song: </label>
+                    <input type="text" class="form-control" id="songSearch" placeholder="Song name">
+                </div>
+                <div class="form-group">
+                    <label for="artistSearch">Artist: </label>
+                    <input type="text" class="form-control" id="artistSearch" placeholder="Artist name">
+                </div>
+                <div class="form-group">
+                    <label for="albumSearch">Album: </label>
+                    <input type="text" class="form-control" id="albumSearch" placeholder="Album name">
+                </div>
+                <div class="form-group">
+                    <label for="genreSearch">Genre: </label>
+                    <input type="text" class="form-control" id="genreSearch" placeholder="Genre">
+                </div>
+                
                 <button type="button" class="btn btn-primary" onclick="searchMusic('generic')"><span class="glyphicon glyphicon-search"></span>  Search</button>
         </form>
     </div>
