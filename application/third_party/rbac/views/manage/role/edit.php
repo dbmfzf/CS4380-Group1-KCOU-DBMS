1f37a20bad6fba641808fe5ed086d9e911a85523
072ashin <leiqing126@gmail.com>
2016-04-14 11:50:48 -0500

    Update edit.php

--- application/third_party/rbac/views/manage/role/edit.php
+++ application/third_party/rbac/views/manage/role/edit.php
@@ -8,6 +8,18 @@
     <label>Role name</label>
     <input name="rolename" type="text" class="form-control" value="<?php echo $data['name']; ?>">
   </div>
+  <div class="form-group">
+    <label>Department</label>
+      <select name="dept" class="form-control" >
+        <?php 
+	    			foreach($dept_data as $key){
+	    			    $select = $data['did']==$key->did?"selected":"";
+	    			    printf('<option value ="%s">%s</oprtion>',$key->did,$key->deptname);
+	    			}
+
+        ?>
+      </select>
+  </div>
   <div class="checkbox">
     <label>
       <input value="1" name="status" type="checkbox" <?php if($data["status"]){echo "checked";}?>> Enable?
