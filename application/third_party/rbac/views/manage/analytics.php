8333559b8fe7790cc370cd6b9c61a06371edbc1c
072ashin <leiqing126@gmail.com>
2016-04-15 10:10:16 -0500

    Update analytics.php

--- application/third_party/rbac/views/manage/analytics.php
+++ application/third_party/rbac/views/manage/analytics.php
@@ -1,5 +1,14 @@
 <?php 
+
           $today = date("Y-m-d");
+          foreach($user_dept_data as $row){
+     	          $dept_cnt[]= $row['user_num'];
+        	          $dept_arr[] = array( 
+                              "name"=> $row['dept_name'],"y"=>intval($row['user_num']) 
+                    ); 
+        	}
+        	$dept_data = json_encode($dept_arr);
+          
      	foreach($user_gender_data as $row){
      	          $gender_cnt[]= $row['user_num'];
         	          $gender_arr[] = array( 
@@ -8,13 +17,15 @@
         	}
         	$gender_data = json_encode($gender_arr);
         	
-        	foreach($user_dept_data as $row){
-     	          $dept_cnt[]= $row['user_num'];
-        	          $dept_arr[] = array( 
-                              "name"=> $row['dept_name'],"y"=>intval($row['user_num']) 
+        	foreach($user_role_data as $row){
+     	          $role_cnt[]= $row['user_num'];
+        	          $role_arr[] = array( 
+                              "name"=> $row['role_name'],"y"=>intval($row['user_num']) 
                     ); 
         	}
-        	$dept_data = json_encode($dept_arr);
+        	$role_data = json_encode($role_arr);
+        	
+
 ?>
 <script>
 $(function () {
@@ -22,7 +33,7 @@ $(function () {
     $(document).ready(function () {
 
         // Build the chart
-        $('#gender').highcharts({
+        $('#dept').highcharts({
             chart: {
                 plotBackgroundColor: null,
                 plotBorderWidth: null,
@@ -30,46 +41,7 @@ $(function () {
                 type: 'pie'
             },
             title: {
-                text: 'Male:<?php echo $gender_cnt[0];?> Female:<?php echo $gender_cnt[1];?>'
-            },
-            subtitle: {
-                text: '<?php echo $today;?>'
-            },
-            tooltip: {
-                 formatter: function() {
-                         return '<b>'+ this.point.name +'</b>: '+ Highcharts.numberFormat(this.y, 0, ',');
-                 }
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: true,
-                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                        style: {
-                              color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                        },
-                        connectorColor: 'silver'
-                    }
-                }
-            },
-            series: [{
-                name: 'Brands',
-                colorByPoint: true,
-                data: <?php echo $gender_data;?>
-            }]
-        });
-        
-        $('#dept').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: true,
-                type: 'pie'
-            },
-            title: {
-                text: 'Department'
+                text: 'KCOU'
             },
             subtitle: {
                 text: '<?php echo $today;?>'
@@ -97,6 +69,84 @@ $(function () {
                 name: 'Brands',
                 colorByPoint: true,
                 data: <?php echo $dept_data;?>
+            }]
+        });
+        
+        $('#role').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: true,
+                type: 'pie'
+            },
+            title: {
+                text: 'KCOU'
+            },
+            subtitle: {
+                text: '<?php echo $today;?>'
+            },
+            tooltip: {
+                 formatter: function() {
+                         return '<b>'+ this.point.name +'</b>: '+ Highcharts.numberFormat(this.y, 0, ',');
+                 }
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        style: {
+                              color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        },
+                        connectorColor: 'silver'
+                    }
+                }
+            },
+            series: [{
+                name: 'Brands',
+                colorByPoint: true,
+                data: <?php echo $role_data;?>
+            }]
+        });
+        
+                $('#gender').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: true,
+                type: 'pie'
+            },
+            title: {
+                text: 'KCOU'
+            },
+            subtitle: {
+                text: '<?php echo $today;?>'
+            },
+            tooltip: {
+                 formatter: function() {
+                         return '<b>'+ this.point.name +'</b>: '+ Highcharts.numberFormat(this.y, 0, ',');
+                 }
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        style: {
+                              color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                        },
+                        connectorColor: 'silver'
+                    }
+                }
+            },
+            series: [{
+                name: 'Brands',
+                colorByPoint: true,
+                data: <?php echo $gender_data;?>
             }]
         });
         
