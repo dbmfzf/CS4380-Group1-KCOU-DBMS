e3e8201c8c3e8707f44aebc0d0deed19b8273135
072ashin <leiqing126@gmail.com>
2016-04-14 21:30:42 -0500

    Update analytics.php

--- application/third_party/rbac/views/manage/analytics.php
+++ application/third_party/rbac/views/manage/analytics.php
@@ -41,27 +41,7 @@ $(function () {
             series: [{
                 name: 'Brands',
                 colorByPoint: true,
-                data: [{
-                    name: 'Microsoft Internet Explorer',
-                    y: 56.33
-                }, {
-                    name: 'Chrome',
-                    y: 24.03,
-                    sliced: true,
-                    selected: true
-                }, {
-                    name: 'Firefox',
-                    y: 10.38
-                }, {
-                    name: 'Safari',
-                    y: 4.77
-                }, {
-                    name: 'Opera',
-                    y: 0.91
-                }, {
-                    name: 'Proprietary or Undetectable',
-                    y: 0.2
-                }]
+                data: <?php echo $arr;>
             }]
         });
     });
