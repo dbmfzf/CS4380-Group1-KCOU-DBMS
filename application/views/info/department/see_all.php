9d8ecf42eb258f26de3873f1312a68686bd84f9e
Keevey <keeveysong@gmail.com>
2016-04-17 13:14:48 -0600

    department

--- application/views/info/department/see_all.php
+++ application/views/info/department/see_all.php
@@ -1,1 +1,33 @@
-<?php
+<style>
+.table td:nth-child(10){width:15%}
+</style>
+<table class="table  table-bordered well">
+	<thead>
+          <tr>
+            <th>Department</th>
+            <th>Description</th>
+            <th>Volunteers</th>
+            <th></th>
+          </tr>
+        </thead>
+   <tbody>
+	<?php 
+	foreach($data as $mb){
+		printf('<tr>
+					<td>%s</td>
+					<td>%s</td>
+					<td>
+						<div class="btn-group  btn-group-xs">
+						  <a class="btn btn-warning btn-xs" href="%s">Edit</a>
+						  <a class="btn btn-danger" href="%s">Delete</a>
+						</div>
+					</td>
+				</tr>',$mb->dname,$mb->description,site_url("info/department/edit/".$mb->did),site_url("info/department/delete/".$mb->did),site_url("info/department/see_all/".$mb->did));
+	}
+	?>
+  </tbody>
+</table>
+<hr/>
+
+<?php echo '<a class="btn btn-success pull-right" href="'.site_url("info/department/add").'">Add new department</a>'; ?>
+<?php echo $this->pagination->create_links(); ?>
