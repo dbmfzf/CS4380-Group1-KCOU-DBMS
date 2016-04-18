cf9429f4cb1151b2f77a3d424768b14984999f05
CaesarQu <caesarqu0630@gmail.com>
2016-04-18 16:51:39 -0500

    Something I built

---
+++ application/controllers/message/messageController.php
@@ -0,0 +1,33 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class messageController extends CI_Controller {
+
+    public function __construct(){
+        parent::__construct();
+        $this->load->model("music_model");
+        $this->load->helper('html');
+        $this->load->library('javascript');
+    }
+    
+    public function index(){
+        $this->load->view("music/search_music.php");
+    }
+    
+    public function genericSearchHandler(){
+        $JSONstring = $this->music_model->genericSearch($this->input->get('searchString'));
+        echo"$JSONstring";
+    }
+    
+    public function advancedSearchHandler(){
+        $JSONArtist = $this->music_model->searchByArtist($this->input->get('artistName'));
+        $JSONAlbum = $this->music_model->searchByAlbum($this->input->get('albumName'));
+        $JSONSong = $this->music_model->searchBySong($this->input->get('songName'));
+        $JSONGenre = $this->music_model->searchByGenre($this->input->get('genreName'));
+        
+        $JSON1 = array_merge($JSONArtist, $JSONAlbum);
+        $JSON2 = array_merge($JSONSong, $JSONGenre);
+        
+        echo(array_merge($JSON1, $JSON2));
+    }
+}
