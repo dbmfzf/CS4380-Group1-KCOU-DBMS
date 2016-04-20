485875347ab5e355a03addfe484dddd7ab150288
Keevey <keeveysong@gmail.com>
2016-04-20 12:56:48 -0600

    department

--- application/controllers/info/department.php
+++ application/controllers/info/department.php
@@ -203,7 +203,7 @@ class Department extends CI_Controller {
 				if($uid!=""){
 					if($uid&&$fullname&&$gender&&$email&&$phone&&$birth&&$role){
 						if($status){$newstat = ",status='1'";}else{$newstat = ",status='0'";}
-						$sql = "UPDATE User set fullname='{$fullname}',gender = '{$gender}',email='{$email}',phone = '{$phone}',birth = '{$birth}',rid='{$role}',{$newstat} WHERE uid = '{$uid}'";
+						$sql = "UPDATE User set fullname='{$fullname}',gender = '{$gender}',email='{$email}',phone = '{$phone}',birth = '{$birth}',rid='{$role}'{$newstat} WHERE uid = '{$uid}'";
 						$this->db->query($sql);
 						success_redirct("info/department/see_all","Edit successful!");
 					}else{
@@ -228,7 +228,7 @@ class Department extends CI_Controller {
 	public function user_delete($uid){
 		$query = $this->db->query("SELECT * FROM user u WHERE u.uid = '".$uid."' ");
 		$data = $query->row_array();
-		$department_query = $this->db->query("SELECT d.did,d.name as dname,d.description, r.rid, r.name as rname, u.uid, u.fullname as uname, u.gender, u.birth, u.email, u.phone FROM department d, role r, user u WHERE and d.did = r.did and r.rid = u.rid and u.uid = '".$uid."' limit 1");
+		$department_query = $this->db->query("SELECT d.did,d.name as dname,d.description, r.rid, r.name as rname, u.uid, u.fullname as uname, u.gender, u.birth, u.email, u.phone FROM department d, role r, user u WHERE d.did = r.did and r.rid = u.rid and u.uid = '".$uid."' limit 1");
 		$department_data = $department_query -> row_array();
 		if($data){
 			if($this->input->post()){
