7fae9320f92fb9898e59dd3145f7b4780acc4006
dbmfzf <dbmfzf@mail.missouri.edu>
2016-04-21 11:25:51 -0500

    fixing controller/model

--- application/controllers/music/search_music.php
+++ application/controllers/music/search_music.php
@@ -25,13 +25,36 @@ class search_music extends CI_Controller {
     //Input: get request from ajax in the search_music view
     //Output: JSON array of song information
     public function advancedSearchHandler(){
+        //run queries or do nothing if the inputs are empty
         $JSONArtist = strlen($this->input->get('artistName')) > 0 ? $this->music_model->searchByArtist($this->input->get('artistName')): "";
         $JSONAlbum = strlen($this->input->get('albumName')) > 0 ? $this->music_model->searchByAlbum($this->input->get('albumName')) : "";
         $JSONSong = strlen($this->input->get('songName')) > 0 ? $this->music_model->searchBySong($this->input->get('songName')) : "";
         $JSONGenre = strlen($this->input->get('genreName')) > 0 ? $this->music_model->searchByGenre($this->input->get('genreName')) : "";
         
+        //Add the arrays to merge to arrsToMerge
+        $arrsToMerge = array();
+        if(!empty($JSONArtist)){
+            $arrsToMerge[] = $JSONArtist;
+        }if(!empty($JSONAlbum)){
+            $arrsToMerge[] = $JSONAlbum;
+        }if(!empty($JSONSong)){
+            $arrsToMerge[] = $JSONSong;
+        }if(!empty($JSONGenre)){
+            $arrsToMerge[] = $JSONGenre;
+        }
         
-        echo(json_encode(array_unique(array_merge_recursive($JSONAlbum, $JSONArtist, $JSONGenre, $JSONSong), SORT_REGULAR)));
+        //If all the arrays are empty then we exit
+        if(sizeof($arrsToMerge) == 0){
+            return;
+        }
+        
+        //Merge all of the query results into $mergedArr
+        $mergedArr = array();
+        for($i = 0; $i < sizeof($arrsToMerge); $i++){
+            $mergedArr = array_merge($mergedArr, $arrsToMerge[$i]);
+        }
+        
+        echo(json_encode(array_unique($mergedArr, SORT_REGULAR)));
         return;
     }
 }
