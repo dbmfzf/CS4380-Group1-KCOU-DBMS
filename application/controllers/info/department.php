9465b9aa37f055ac43b644527bbf0d45600da475
Keevey <keeveysong@gmail.com>
2016-04-22 12:46:03 -0600

    department

--- application/controllers/info/department.php
+++ application/controllers/info/department.php
@@ -231,8 +231,7 @@ class Department extends CI_Controller {
 		$current_role_dept_data = $current_role_dept_query -> row_array();
 	
 		$role_dept_query = $this->db->query("SELECT r.rid from role r WHERE r.did = '".$did."'");
-		$role_dept_data = $role_dept_query->row_array();
-		$rid = $role_dept_data['rid'];
+		$role_dept_data = $role_dept_query->result();
 		if($this->input->post()){
 			$uid = $this->input->post("uid");
 			$uname = $this->input->post("fullname");
