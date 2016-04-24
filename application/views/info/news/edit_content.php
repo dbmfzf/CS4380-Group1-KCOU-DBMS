63cb107ffdd0f40a39a463ca442b7f7f7463e2c7
072ashin <leiqing126@gmail.com>
2016-04-23 19:06:48 -0500

    Update edit_content.php

--- application/views/info/news/edit_content.php
+++ application/views/info/news/edit_content.php
@@ -2,12 +2,14 @@
 <script type="text/javascript" src="<?php echo base_url();?>static/ueditor/ueditor.config.js"></script>
 <script type="text/javascript" src="<?php echo base_url();?>static/ueditor/ueditor.all.js"></script>
 <script type="text/javascript" src="<?php echo base_url();?>static/ueditor/lang/en/en.js"></script>
-<script type="text/javascript">UE.getEditor('container');
+<script type="text/javascript">
+var editor = new baidu.editor.ui.Editor();
+editor.render("myEditor");
 </script>    
 
 <h1>News Content</h1>
-<form action="" method="post"> 
-	<script id="container" name="content" type="text/plain" ><?php echo $news_data['content'];?></script>
+<form action="" method="post">
+	<textarea  name="content" id="myEditor" ><?php echo $news_data['content'];?></textarea>
 
     <button type="submit" class="btn btn-success">Submit</button> 
     <a class="btn btn-danger" href="<?php echo site_url('info/news/index'); ?>">Cancel</a> 
