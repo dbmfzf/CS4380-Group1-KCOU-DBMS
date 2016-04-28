257299bfaa3a0170ebf9348dc7c5f31007bbc67b
Keevey <keeveysong@gmail.com>
2016-04-28 18:40:59 -0500

    Update news.php

--- application/controllers/info/news.php
+++ application/controllers/info/news.php
@@ -163,7 +163,7 @@ class news extends CI_Controller {
 
 	public function analysis()
 	{
-		$news_query = $this->db->query("SELECT n.nid, count(*) as news_num FROM submits s, user u where u.uid = s.uid GROUP BY s.uid");
+		$news_query = $this->db->query("SELECT u.fullname, count(*) as news_num FROM submits s, user u where u.uid = s.uid GROUP BY s.uid");
 		$news_data = $user_dept_query->result_array();
 		
 		
