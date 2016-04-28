7edee76783135143a71c6cd000b05f2a6bc10043
Keevey <keeveysong@gmail.com>
2016-04-28 18:40:14 -0500

    Update analysis.php

--- application/views/info/news/analysis.php
+++ application/views/info/news/analysis.php
@@ -1,8 +1,7 @@
 <?php 
 
-          $today = date("Y-m-d");
-          foreach($user_dept_data as $row){
-     	          $dept_cnt[]= $row['user_num'];
+          foreach($news_data as $row){
+     	          $news_count[]= $row['news_num'];
         	          $dept_arr[] = array( 
                               "name"=> $row['dept_name'],"y"=>intval($row['user_num']) 
                     ); 
