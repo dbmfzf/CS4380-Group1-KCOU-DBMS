369ba6993eda8e769f7a93440ab2fafb5076b5ad
072ashin <leiqing126@gmail.com>
2016-04-29 16:20:48 -0500

    Update analysis.php

--- application/views/info/news/analysis.php
+++ application/views/info/news/analysis.php
@@ -1,27 +1,23 @@
 <?php 
 
-          foreach($news_data as $row){
-     	          $news_count[]= $row['news_num'];
-     	          $user_name[]= $row['fullname'];
-     	          $name = $row['fullname'];
-        	          $news_arr[] = array( 
-                               $row['fullname'],  intval($row['news_num']) 
-                    ); 
-        	}
-        	$news_data = json_encode($news_arr);
-        	$news_cnt[]= json_encode($news_count);
-     	$volunteer_name[]= json_encode($user_name);
-        	
-
+foreach($news_data as $row){
+               $news_count[]= $row['news_num'];
+               $user_name[]= $row['fullname'];
+               $name = $row['fullname'];
+                  $news_arr[] = array( 
+                     $row['fullname'],  intval($row['news_num']) 
+          ); 
+        }
+        $news_data = json_encode($news_arr);
+        $news_cnt[]= json_encode($news_count);
+        $volunteer_name[]= json_encode($user_name);
+        echo $news_arr;
 ?>
-
-<script type="text/javascript" src="<?php echo base_url();?>static/highcharts/js/barchart.js"></script>
-<script type="text/javascript" src="<?php echo base_url();?>static/highcharts/js/exporting.js"></script>
 
 <div id="container" style="min-width: 300px; height: 400px; margin: 0 auto"></div>
 
 <script>
-var arr = <?php echo $news_data;?>;
+  var arr = <?php echo $news_data;?>;
   var ds=[]
   for(var k in arr){
       ds.push( typeof(arr[k])=='object'?arr[k]:[k,arr[k]])
@@ -31,6 +27,7 @@ var arr = <?php echo $news_data;?>;
 //while (newArr.length > 0) {
   //  newArr.pop();
 //}
+/*
 $(function () {
     $('#container').highcharts({
         chart: {
@@ -83,7 +80,8 @@ $(function () {
         }]
     });
 });
-/*
+*/
+
 $(function () {
     $('#container').highcharts({
         chart: {
@@ -121,25 +119,7 @@ $(function () {
             name: 'Population',
             data: [
                 ['Shanghai', 23.7],
-                ['Lagos', 16.1],
-                ['Istanbul', 14.2],
-                ['Karachi', 14.0],
-                ['Mumbai', 12.5],
-                ['Moscow', 12.1],
-                ['São Paulo', 11.8],
-                ['Beijing', 11.7],
-                ['Guangzhou', 11.1],
-                ['Delhi', 11.1],
-                ['Shenzhen', 10.5],
-                ['Seoul', 10.4],
-                ['Jakarta', 10.0],
-                ['Kinshasa', 9.3],
-                ['Tianjin', 9.3],
-                ['Tokyo', 9.0],
-                ['Cairo', 8.9],
-                ['Dhaka', 8.9],
-                ['Mexico City', 8.9],
-                ['Lima', 8.9]
+                ['Lagos', 16.1]
             ],
             dataLabels: {
                 enabled: true,
@@ -156,6 +136,6 @@ $(function () {
         }]
     });
 });
-*/
+
 </script>
 
