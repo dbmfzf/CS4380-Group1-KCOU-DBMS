6585955ebbbeca694caa4f600dd17b58cb4b64b2
Keevey <keeveysong@gmail.com>
2016-04-28 19:18:51 -0500

    Update analysis.php

--- application/views/info/news/analysis.php
+++ application/views/info/news/analysis.php
@@ -15,61 +15,7 @@
 <script type="text/javascript" src="<?php echo base_url();?>static/highcharts/js/exporting.js"></script>
 
 <div id="container" style="min-width: 300px; height: 400px; margin: 0 auto"></div>
-/*
-<script>
-$(function () {
-    $('#container').highcharts({
-        chart: {
-            type: 'column'
-        },
-        title: {
-            text: 'Top Contributors'
-        },
-        xAxis: {
-            labels: {
-                rotation: -45,
-                style: {
-                    fontSize: '13px',
-                    fontFamily: 'Verdana, sans-serif'
-                }
-            }
-            title: {
-                text: 'Name'
-            }
-        },
-        yAxis: {
-            min: 0,
-            title: {
-                text: 'News Submitted'
-            }
-        },
-        legend: {
-            enabled: false
-        },
-        tooltip: {
-            pointFormat: 'This volunteer submitted <b>{point.y:.1f} piece(s) of news</b>'
-        },
-        series: [{
-            name: 'News submission',
-            data: [<?php echo $news_data;?>
-            ],
-            dataLabels: {
-                enabled: true,
-                rotation: -90,
-                color: '#FFFFFF',
-                align: 'right',
-                format: '{point.y:.1f}', // one decimal
-                y: 10, // 10 pixels down from the top
-                style: {
-                    fontSize: '13px',
-                    fontFamily: 'Verdana, sans-serif'
-                }
-            }
-        }]
-    });
-});
-</script>
-*/
+
 $(function () {
     $('#container').highcharts({
         chart: {
