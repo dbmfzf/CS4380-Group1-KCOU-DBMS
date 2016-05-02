5313f6376f9f402afa21461fb38622397f765bfa
072ashin <leiqing126@gmail.com>
2016-05-01 20:58:40 -0500

    Update edit.php

--- application/views/playlist/edit.php
+++ application/views/playlist/edit.php
@@ -1,12 +1,11 @@
 <style>
 .tableleft{font-weight:bold;background-color:#F5F5F5;}
-.textarea{ width:500px; height:250px}
 </style>
 <h1>Edit Playlist</h1>
 <form action="" method="post"> 
     <table class="table table-bordered table-hover definewidth m10">
         <tr>
-            <td width = "15%" class="tableleft">Department</td>
+            <td width = "20%" class="tableleft">Playlist's name</td>
             <td><input type="text" name="name" value="<?php echo $name_data['name'];?>"></td>
         </tr>
     </table>
