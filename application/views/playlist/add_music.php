8f3c1667039ede5c261e4e6d04850d6400cde29e
072ashin <leiqing126@gmail.com>
2016-05-03 16:27:59 -0500

    Create add_music.php

---
+++ application/views/playlist/add_music.php
@@ -0,0 +1,26 @@
+ <style>
+.tableleft{font-weight:bold;background-color:#F5F5F5;}
+</style>
+<h1>Add <?php echo $song_data['title'];?> to a playlist</h1>
+<form action="" method="post"> 
+
+        <tr>
+            <td width = "15%" class="tableleft">Playlist</td>
+            <td>
+              <select name="pid">
+              <?php
+		    		        $rid = $data['rid'];
+		    		        $rolename = $data['rolename'];
+		    		        $deptname = $data['deptname'];
+			    		      foreach($playlist_data as $key){
+					  	          printf('<option value = "%s"> %s </option>',$key->pid,$key->name);
+			    		      }
+		    	    ?>
+	        	  </select>
+		      </td>
+        </tr>
+
+    </table>
+    <button type="submit" class="btn btn-success">Submit</button> 
+     <a class="btn btn-danger" href="<?php echo site_url('info/news/index'); ?>">Cancel</a> 
+</form>
