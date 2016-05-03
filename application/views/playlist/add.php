084a652183067c0368e31283e9955ec2457db2f1
072ashin <leiqing126@gmail.com>
2016-05-02 20:21:52 -0500

    Update add.php

--- application/views/playlist/add.php
+++ application/views/playlist/add.php
@@ -1,5 +1,6 @@
 <style>
 .tableleft{font-weight:bold;background-color:#F5F5F5;}
+.textarea{ width:500px; height:250px}
 </style>
 <h1>Add new playlist</h1>
 <form action="" method="post"> 
@@ -8,7 +9,10 @@
             <td class="tableleft">Name</td>
             <td><input type="text" name="name"</td>
       </tr>
-        
+     <tr>
+            <td class="tableleft">Memo</td>
+            <td><textarea class= "textarea" name="memo" > <?php echo $data['memo'];?></textarea></td>
+      </tr> 
     </table>
     <button type="submit" class="btn btn-success">Save</button> 
      <a class="btn btn-danger" href="<?php echo site_url('playlist/playlist/index'); ?>">Cancel</a> 
