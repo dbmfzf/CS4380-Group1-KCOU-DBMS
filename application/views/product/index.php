23ebaf5ed815a804aff6bf8ffef1cf827b7f7543
072ashin <leiqing126@gmail.com>
2016-05-02 21:43:32 -0500

    Update index.php

--- application/views/product/index.php
+++ application/views/product/index.php
@@ -1,6 +1,3 @@
-<style>
-     .img{width:100%}
-</style>
 <p class="pull-right visible-xs">
             <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
           </p>
