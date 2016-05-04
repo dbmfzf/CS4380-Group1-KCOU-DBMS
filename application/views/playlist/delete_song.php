21df3aa52be5bedd91d88415550dba68a83b4f88
072ashin <leiqing126@gmail.com>
2016-05-03 20:19:28 -0500

    Update delete_song.php

--- application/views/playlist/delete_song.php
+++ application/views/playlist/delete_song.php
@@ -7,6 +7,6 @@
 <form method="POST" action="">
 	<input type="hidden" name="verfiy" value="1" >
 	<input class="btn btn-success"  type="submit" value="OK">
-	<a class="btn btn-danger" href="<?php echo site_url('playlist/playlist/delete_song/'.$data["sid"]); ?>">Cancel</a>
+	<a class="btn btn-danger" href="<?php echo site_url('playlist/playlist/index); ?>">Cancel</a>
 </form>
 </hr>
