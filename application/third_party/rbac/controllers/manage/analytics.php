f0a3c66bbab7e63194dc19f697a622b9476d34b7
072ashin <leiqing126@gmail.com>
2016-05-03 21:55:55 -0500

    Update analytics.php

--- application/third_party/rbac/controllers/manage/analytics.php
+++ application/third_party/rbac/controllers/manage/analytics.php
@@ -29,7 +29,7 @@ class Analytics extends CI_Controller {
 		} 
 		$date_string = implode(",", $date);
 		echo $date_string;
-		$query=$this->db->query("SELECT count(*) as cnt,substr(date_time,1,10) AS login_date FROM login_record WHERE login_date IN ($date_string) GROUP BY login_date ORDER BY login_date");
+		$query=$this->db->query("SELECT count(*) as cnt,substr(date_time,1,10) as login_date FROM login_record WHERE substr(date_time,1,10) IN ($date_string) GROUP BY substr(date_time,1,10) ORDER BY substr(date_time,1,10)");
 		$data = $query->result_array();
 		$this->load->view("manage/about_usage",array("data"=>$data));
 		
