79f40d00baeb20a32bed274729a837036fcc1aa7
072ashin <leiqing126@gmail.com>
2016-05-03 22:20:20 -0500

    Update about_usage.php

--- application/third_party/rbac/views/manage/about_usage.php
+++ application/third_party/rbac/views/manage/about_usage.php
@@ -1,9 +1,10 @@
 <?php 
 	foreach($data as $row){
 		$login_arr[] = $row["login_date"];
-		$count[] = intval($row["cnt"]);
+		$count_arr[] = intval($row["cnt"]);
 	}
 	$login_date = json_encode($login_arr);
+	$login_cnt = json_encode($count_arr);
 ?>
 <h1>Usage tracking for the previous 7(at most) days</h1>
 <div align ="center" id="container" style="min-width: 600px; height: 450px; max-width: 800px; margin: 0 auto; padding-top:5%"></div>
@@ -42,7 +43,7 @@ $(function () {
         },
         series: [{
             name: 'Tokyo',
-            data: 
+            data: <?php echo $login_cnt; ?>
         }]
     });
 });
