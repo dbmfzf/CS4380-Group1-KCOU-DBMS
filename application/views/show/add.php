cad22edd44370aa5bf90315266e45425c72e99e1
CaesarQu <caesarqu0630@gmail.com>
2016-05-04 02:24:28 -0500

    friday

--- application/views/show/add.php
+++ application/views/show/add.php
@@ -139,7 +139,7 @@ function getWeekDay(){
 					<option value="Tuesday">Tuesday</option>
 					<option value="Wednesday">Wednesday</option>
 					<option value="Thursday">Thursday</option>
-					<option value="Firday">Firday</option>
+					<option value="Friday">Friday</option>
 					<option value="Saturday">Saturday</option>
 					<option value="Sunday">Sunday</option>
 				</select></br></td>
