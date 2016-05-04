000f8150c6b01c286da80755eb4434f72986ba3d
072ashin <leiqing126@gmail.com>
2016-05-03 21:51:08 -0500

    Update about_usage.php

--- application/third_party/rbac/views/manage/about_usage.php
+++ application/third_party/rbac/views/manage/about_usage.php
@@ -1,8 +1,5 @@
-<?php for($i=1;$i<=7;$i++){ 
-			$temp_date = date("Y-m-d",strtotime("-$i day"));
- 			$date[]= "'{$temp_date}'";
- 			
-		} 
-			$date_string = implode(",", $date);
-			echo $date_string;
+<?php 
+	foreach($data as $row){
+		echo $data['login_date'];
+	}
 ?>
