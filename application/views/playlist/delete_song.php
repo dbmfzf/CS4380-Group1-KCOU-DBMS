4e9cbfe21437b814077123699ae7ae6898443fc7
072ashin <leiqing126@gmail.com>
2016-05-03 19:03:29 -0500

    Update delete_song.php

--- application/views/playlist/delete_song.php
+++ application/views/playlist/delete_song.php
@@ -2,10 +2,10 @@
 .table td:first-child{width:30%}
 .table td:nth-child(2){width:40%}
 </style>
-<h1>Are you sure to delete this song(<?php echo $data["title"]; ?>)?</h1>
+<h1>Are you sure to delete this song(<?php echo $data["title"];$sid =$data["sid"]; ?>)?</h1>
 
 <form method="POST" action="">
 	<input type="hidden" name="verfiy" value="1" >
 	<input class="btn btn-success"  type="submit" value="OK">
-	<a class="btn btn-danger" href="<?php echo site_url('playlist/playlist/see_all_songs/".$data["sid"]'); ?>">Cancel</a>
+	<a class="btn btn-danger" href="<?php echo site_url('playlist/playlist/see_all_songs/'{$sid}''); ?>">Cancel</a>
 </form>
