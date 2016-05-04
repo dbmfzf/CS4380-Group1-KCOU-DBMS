aa4e564ef23d29d9d11aef5fc25a866930153406
072ashin <leiqing126@gmail.com>
2016-05-03 22:19:25 -0500

    Update about_usage.php

--- application/third_party/rbac/views/manage/about_usage.php
+++ application/third_party/rbac/views/manage/about_usage.php
@@ -1,10 +1,11 @@
 <?php 
 	foreach($data as $row){
 		$login_arr[] = $row["login_date"];
+		$count[] = intval($row["cnt"]);
 	}
 	$login_date = json_encode($login_arr);
 ?>
-<h1>Usage tracking for the previous 7 days</h1>
+<h1>Usage tracking for the previous 7(at most) days</h1>
 <div align ="center" id="container" style="min-width: 600px; height: 450px; max-width: 800px; margin: 0 auto; padding-top:5%"></div>
 <script>
 $(function () {
@@ -22,7 +23,7 @@ $(function () {
         },
         yAxis: {
             title: {
-                text: 'Temperature (°C)'
+                text: 'Login Record'
             },
             plotLines: [{
                 value: 0,
@@ -31,7 +32,7 @@ $(function () {
             }]
         },
         tooltip: {
-            valueSuffix: '°C'
+            valueSuffix: 'item(s)'
         },
         legend: {
             layout: 'vertical',
@@ -41,16 +42,7 @@ $(function () {
         },
         series: [{
             name: 'Tokyo',
-            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2,]
-        }, {
-            name: 'New York',
-            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8]
-        }, {
-            name: 'Berlin',
-            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6]
-        }, {
-            name: 'London',
-            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0]
+            data: 
         }]
     });
 });
