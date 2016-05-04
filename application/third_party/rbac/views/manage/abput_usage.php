3a9c52e67d7192e8d964171fd1959be9ba595008
072ashin <leiqing126@gmail.com>
2016-05-03 21:38:28 -0500

    Update abput_usage.php

--- application/third_party/rbac/views/manage/abput_usage.php
+++ application/third_party/rbac/views/manage/abput_usage.php
@@ -1,4 +1,7 @@
 <?php for($i=1;$i<=7;$i++){ 
- 			echo date("Y-m-d",strtotime("-$i day")); 
+ 			$date[]=date("Y-m-d",strtotime("-$i day"));
+ 			
 		} 
+			$date_string = implode("OR date_time = ", '".$date."');
+			echo $date_string;
 ?>
