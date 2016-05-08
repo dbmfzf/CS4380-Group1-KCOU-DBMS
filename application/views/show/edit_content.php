b2b76baa936c4f50e70d3c6fd23858012afce6bf
072ashin <leiqing126@gmail.com>
2016-05-08 14:43:01 -0500

    Update edit_content.php

--- application/views/show/edit_content.php
+++ application/views/show/edit_content.php
@@ -2,6 +2,7 @@
 <form action="" method="post">
 	<textarea style="padding-bottom:10px" name="content" ><?php echo $shows_data['content'];?></textarea>
 
+    <br/>
     <button type="submit" class="btn btn-success">Submit</button> 
     <a class="btn btn-danger" href="<?php echo site_url('show/showController/index'); ?>">Cancel</a> 
 </form>
