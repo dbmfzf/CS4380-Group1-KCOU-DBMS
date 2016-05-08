4b58542eec597ec53df16bd14d39c0da0736ca2e
072ashin <leiqing126@gmail.com>
2016-05-08 14:41:49 -0500

    Update edit_content.php

--- application/views/show/edit_content.php
+++ application/views/show/edit_content.php
@@ -1,15 +1,6 @@
-
-<script type="text/javascript" src="<?php echo base_url();?>static/ueditor/ueditor.config.js"></script>
-<script type="text/javascript" src="<?php echo base_url();?>static/ueditor/ueditor.all.js"></script>
-<script type="text/javascript" src="<?php echo base_url();?>static/ueditor/lang/en/en.js"></script>
-<script type="text/javascript">
-var editor = new UE.ui.Editor({initialFrameHeight:600});
-editor.render("myEditor");
-</script>    
-
-<h1>News Content</h1>
+<h1>Show's description</h1>
 <form action="" method="post">
-	<textarea style="padding-bottom:10px"  name="content" id="myEditor" ><?php echo $shows_data['content'];?></textarea>
+	<textarea style="padding-bottom:10px" name="content" ><?php echo $shows_data['content'];?></textarea>
 
     <button type="submit" class="btn btn-success">Submit</button> 
     <a class="btn btn-danger" href="<?php echo site_url('show/showController/index'); ?>">Cancel</a> 
